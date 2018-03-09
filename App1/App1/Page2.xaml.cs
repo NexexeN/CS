5a6779f028a7bd7d6a3aaee19917d008b2baf944
NexexeN <34157423+NexexeN@users.noreply.github.com>
2018-03-09 11:01:35 +0100

    3

--- App1/App1/Page2.xaml.cs
+++ App1/App1/Page2.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App1
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Page2 : Page
     {
-        public MainPage()
+        public Page2()
         {
             this.InitializeComponent();
-        }
-
-        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Validation(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Page2));
         }
     }
 }
